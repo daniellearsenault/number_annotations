16d4b8c8ace919cc8394ed9e53a60843b5862e28
danielle arsenault <56440050+daniellearsenault@users.noreply.github.com>
2022-05-21 19:38:07 -0400

    Create number_annotations.py

---
+++ number_annotations.py
@@ -0,0 +1,16 @@
+filename = raw_input("Enter fasta filename to add unique number to beginning of each annotation line:\n")
+outname = raw_input("What would you like to name your numbered output file?")
+
+with open(filename,'r') as file_raw:
+    file = file_raw.read()
+
+output = open(outname,'w')
+
+counter = 0
+for c in file:
+    output.write(c)
+    if c == '>':
+        output.write(str(counter))
+        counter += 1
+
+output.close()
